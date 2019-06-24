53767012719ce44b48ae30424b060cee99f1fb0a
Stefan Beader <stefan.beder@stanga.net>
2019-06-24 16:22:12 +0200

    Sociallite

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('login/google', 'Auth\LoginController@redirectToProvider');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::namespace('Frontend')->group(function () {
+
+    Route::get('/', 'PagesController@home');
+
+});
+
+Route::group(['namespace' => 'Backend', 'middleware' => 'auth'], function () {
+
+    Route::get('/admin', 'AdminController@dashboard');
+
 });
